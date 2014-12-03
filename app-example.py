b65f6588f58293b57943aea9e1ef7a7ab777eb12
David Rossellat <david.rossellat@gmail.com>
2014-12-03 15:40:35 +0700

    adding user id support

--- app-example.py
+++ app-example.py
@@ -10,13 +10,16 @@ from oauth2client.client import AccessTokenRefreshError
 
 # BigQuery API Settings
 SCOPE = 'https://www.googleapis.com/auth/bigquery'
-PROJECT_NUMBER = 'xxxxxxxxxxxxxxxx' # REPLACE WITH YOUR Project ID
+PROJECT_NUMBER = 'acquired-vector-747' # REPLACE WITH YOUR Project ID
 
 # Create a new API service for interacting with BigQuery
 credentials = AppAssertionCredentials(scope=SCOPE)
 http = credentials.authorize(httplib2.Http())
 bigquery_service = build('bigquery', 'v2', http=http)
 
+class NothingToSee(webapp.RequestHandler):
+   def get(self):
+      self.response.out.write('oui.')
 
 class ListDatasets(webapp.RequestHandler):
   def get(self):
@@ -35,10 +38,31 @@ class ListDatasets(webapp.RequestHandler):
 
 class ExportTable(webapp.RequestHandler):
    def get(self):
-     projectId = 'xxxxxxxxxxxxxxxx'
-     datasetId = "xxxxxxxxxxxxxxxx"
-     tableId = 'xxxxxxxxxxxxxxxx'
-     query = "SELECT REGEXP_EXTRACT(user_agent, r'(1.0.\d+)') AS build_version, controller + ':' + action  AS controller_action, time FROM [" + projectId + ":" + datasetId + "." + tableId + "]" 
+     projectId = "xxxxxxxxxxx"
+     datasetId = "xxxxxxxxxxx"
+     #query = "SELECT REGEXP_EXTRACT(user_agent, r'(1.0.\d+)') AS build_version, controller + ':' + action  AS controller_action, time FROM [" + projectId + ":" + datasetId + "." + tableId + "]" 
+     query=("select us0.id AS user,tmpx.buildversion AS build, tmpx.day_of_time AS time, tmpx.controlleraction AS action FROM [" + projectId + ":" + datasetId + ".xxxxxxxxxxx] AS us0"
+            " JOIN EACH "
+            "(SELECT "
+            "oa0.resource_owner_id AS resource_owner_id,"
+            "STRFTIME_UTC_USEC"
+            " (tmp.time,'%Y-%m-%d %H:%M') AS day_of_time,"
+            "tmp.buildversion AS buildversion "
+            "FROM "
+            "(SELECT "
+            "REGEXP_EXTRACT(lo0.Authorization, r'Bearer (\w+)') AS token,"
+            "REGEXP_EXTRACT(lo0.user_agent, r'^Game SDK/(1.0.\d+)') AS buildversion,"
+            "lo0.time AS time,(lo0.controller + ':' + lo0.action) AS controllerAction"
+            " FROM [" + projectId + ":" + datasetId + ".xxxxxxxxxxx] AS lo0"
+            ") AS tmp"
+            " JOIN EACH [" + projectId + ":" + datasetId + ".xxxxxxxxxxx] AS oa0"
+            " ON tmp.token = oa0.token"
+            " GROUP BY resource_owner_id, buildversion,day_of_time, controlleraction"
+            ") AS tmpx"
+            " ON us0.id = tmpx.resource_owner_id"
+            )
+
+
      url = "https://www.googleapis.com/bigquery/v2/projects/" + projectId + "/jobs"
 
      jobCollection = bigquery_service.jobs()
@@ -50,7 +74,7 @@ class ExportTable(webapp.RequestHandler):
                "destinationTable":{
                   "projectId": projectId,
                   "datasetId": datasetId,
-                  "tableId": "limited_log"
+                  "tableId": "xxxxxxxxxxx"
                   },
                   "writeDisposition": "WRITE_TRUNCATE"
             }
@@ -69,9 +93,11 @@ class ExportTable(webapp.RequestHandler):
        time.sleep(10)
 
 app = webapp.WSGIApplication(
-    [('/listdatasets', ListDatasets),('/export', ExportTable)],
+    [('/listdatasets', ListDatasets),('/export', ExportTable), ('/', NothingToSee)],
     debug=True)
 
 def main():
   run_wsgi_app(app)
+
+
 
